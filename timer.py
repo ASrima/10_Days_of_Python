15446fae2bc374f664c0749cd98b215463a453bd
Ayesha <ayeshasultanarima@gmail.com>
2019-01-09 15:23:12 -0500

    Add files via upload
    A simple python program that can track the time

---
+++ timer.py
@@ -0,0 +1,21 @@
+import os
+import time
+
+hours = 0
+minutes = 0
+seconds = 0
+
+start = raw_input("Enter r to run the program: ") #in python 3 use input instead of raw_input
+
+while start.lower() =="r":
+    if seconds > 59:
+        seconds = 0 #restes the seconds
+        minutes = minutes + 1
+    if minutes >59:
+        minutes = 0
+        hours = hours + 1
+    os.system('clear') #for windows use cls
+
+    seconds = (seconds + .1)
+    print (hours, ":", minutes,":", seconds,":")
+    time.sleep(.1) #every .1 seconds it will go back up and clear the screen
